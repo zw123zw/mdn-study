af191e291f95134af0cf8fac79e36bb728579817
zw123zw <zw18674069215@163.com>
2022-03-12 08:51:01 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 console.log('Hello MDN')
 
-const a = 13
-const b = a
-console.log(a)
-console.log(b)
+const a = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(111)
+    }, 2000)
+  })
+a().then((res) => {
+  console.log(res)
+})
+
+async function dd() {
+  await a()
+  console.log(123)
+}
+
+dd()
+
+const b = new Promise((resolve, reject) => {
+  resolve(222)
+})
+b.then((res) => {
+  console.log(res)
+})
