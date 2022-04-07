001e1cd45fb6c978096a8b4839e98b6f3c63d3a0
zw123zw <zw18674069215@163.com>
2022-04-07 22:31:15 +0800

    mdn学习storge

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const logNext = (...str) => {
-  window.console.clear()
   window.console.log(...str)
+  window.console.log('--------------------------------')
 }
 
 logNext('Hello MDN', 123)
@@ -199,3 +199,87 @@ function logSelection(event) {
 
 const input = document.querySelector('input')
 input.addEventListener('select', logSelection)
+
+var dragged
+
+/* 拖动目标元素时触发drag事件 */
+document.addEventListener('drag', function (event) {}, false)
+
+document.addEventListener(
+  'dragstart',
+  function (event) {
+    // 保存拖动元素的引用(ref.)
+    dragged = event.target
+    // 使其半透明
+    event.target.style.opacity = 0.5
+  },
+  false
+)
+
+document.addEventListener(
+  'dragend',
+  function (event) {
+    // 重置透明度
+    event.target.style.opacity = ''
+  },
+  false
+)
+
+/* 放置目标元素时触发事件 */
+document.addEventListener(
+  'dragover',
+  function (event) {
+    // 阻止默认动作以启用drop
+    event.preventDefault()
+  },
+  false
+)
+
+document.addEventListener(
+  'dragenter',
+  function (event) {
+    // 当可拖动的元素进入可放置的目标时高亮目标节点
+    if (event.target.className == 'dropzone') {
+      event.target.style.background = 'purple'
+    }
+  },
+  false
+)
+
+document.addEventListener(
+  'dragleave',
+  function (event) {
+    // 当拖动元素离开可放置目标节点，重置其背景
+    if (event.target.className == 'dropzone') {
+      event.target.style.background = ''
+    }
+  },
+  false
+)
+
+document.addEventListener(
+  'drop',
+  function (event) {
+    // 阻止默认动作（如打开一些元素的链接）
+    event.preventDefault()
+    // 将拖动的元素到所选择的放置目标节点中
+    if (event.target.className == 'dropzone') {
+      event.target.style.background = ''
+      dragged.parentNode.removeChild(dragged)
+      event.target.appendChild(dragged)
+    }
+  },
+  false
+)
+
+window.addEventListener('storage', () => {
+  // When local storage changes, dump the list to
+  // the console.
+  console.log(JSON.parse(window.localStorage.getItem('sampleList')))
+})
+
+window.onstorage = () => {
+  // When local storage changes, dump the list to
+  // the console.
+  console.log(JSON.parse(window.localStorage.getItem('sampleList')))
+}
